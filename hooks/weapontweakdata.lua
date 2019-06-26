4eead74e6ef6e21792925947a4331393c5f4fc31
GermanTacos <jakebuilds@gmail.com>
2019-06-25 22:08:55 -0400

    nam -> psa_nam

--- hooks/weapontweakdata.lua
+++ hooks/weapontweakdata.lua
@@ -1,39 +1,39 @@
 Hooks:PostHook( WeaponTweakData, "init", "PSACommandomodInit", function(self)
 
-if ( self.nam ) then
+if ( self.psa_nam ) then
 --- Base stuff ---	
-	self:SetupAttachmentPoint("nam", {
-		name = "a_ns", 
+	self:SetupAttachmentPoint("psa_nam", {
+		psa_name = "a_ns", 
         base_a_obj = "a_ns", 
         position = Vector3( 0, -16.6, 0.2 ), -- -16
         rotation = Rotation( 0, 0, 0 ) 
 	})
-	self:SetupAttachmentPoint("nam", {
-		name = "a_ns_df", 
+	self:SetupAttachmentPoint("psa_nam", {
+		psa_name = "a_ns_df", 
         base_a_obj = "a_ns", 
         position = Vector3( 0, -8, 2 ), -- -16
         rotation = Rotation( 0, 0, 0 ) 
 	})
-	self:SetupAttachmentPoint("nam", {
-		name = "a_fl", 
+	self:SetupAttachmentPoint("psa_nam", {
+		psa_name = "a_fl", 
         base_a_obj = "a_fl", 
         position = Vector3( 0.2, -2, 0.5 ), 
         rotation = Rotation( 0, 0, 0 ) 
 	})
-	self:SetupAttachmentPoint("nam", {
-		name = "a_gl", 
+	self:SetupAttachmentPoint("psa_nam", {
+		psa_name = "a_gl", 
         base_a_obj = "a_fl", 
         position = Vector3( 0, 0, 0 ), 
         rotation = Rotation( 0, 0, 0 ) 
 	})
-	self:SetupAttachmentPoint("nam", {
-		name = "a_o", 
+	self:SetupAttachmentPoint("psa_nam", {
+		psa_name = "a_o", 
         base_a_obj = "a_o", 
         position = Vector3( 0, -2, 0 ), 
         rotation = Rotation( 0, 0, 0 ) 
 	})
-	self:SetupAttachmentPoint("nam", {
-		name = "a_os", 
+	self:SetupAttachmentPoint("psa_nam", {
+		psa_name = "a_os", 
         base_a_obj = "a_os", 
         position = Vector3( 0, 0, 0 ), 
         rotation = Rotation( 0, 0, 0 )
