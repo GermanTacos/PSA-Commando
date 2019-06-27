568d6380c873ede8d31acd74e6761ddc0a2bc677
IKillerwolfI <killerwolf.no1@gmail.com>
2019-06-27 09:40:28 +0200

    Sight Stances V1

--- hooks/weaponfactorytweakdata.lua
+++ hooks/weaponfactorytweakdata.lua
@@ -1,4 +1,12 @@
-Hooks:PostHook( WeaponFactoryTweakData, "init", "PSASF15modInit", function(self)
+Hooks:PostHook( WeaponFactoryTweakData, "init", "PSACommandomodInit", function(self)
 		
-
+-- Modified Pawcio code
+for _, sight_id in pairs(self.wpn_fps_ass_psa_nam.uses_parts) do
+	if self.parts[sight_id].type == "sight" then
+		if self.parts[sight_id].stance_mod.wpn_fps_ass_m16 then
+			self.parts[sight_id].stance_mod.wpn_fps_ass_psa_nam = deep_clone(self.parts[sight_id].stance_mod.wpn_fps_ass_m16)
+		--	self.wpn_fps_ass_psa_nam.adds[sight_id] = {"wpn_fps_smg_ppsh_sight_rail"}
+		end
+	end
+end
 end )
